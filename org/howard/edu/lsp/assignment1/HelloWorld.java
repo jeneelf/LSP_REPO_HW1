7e406de3c662c9e03118cb6b4eef657148f8ad74
jeneelf <jeneelfarrell@gmail.com>
2025-01-30 15:56:35 -0500

    Initial commit: HW1 HelloWorld program

---
+++ org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Jeneel Farrell
+ * @03023942
+ * HW Assignment 1
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
